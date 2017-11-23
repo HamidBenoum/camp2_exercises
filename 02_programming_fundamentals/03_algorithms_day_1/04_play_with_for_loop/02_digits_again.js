43e801f71ab6ac16927497f38e8fe5b2d3236ed1
Hamid Ben oumaizina <hamid.benoumaizina@decathlon.com>
2017-11-23 18:56:13 +0100

    mes exo du soir

--- 02_programming_fundamentals/03_algorithms_day_1/04_play_with_for_loop/02_digits_again.js
+++ 02_programming_fundamentals/03_algorithms_day_1/04_play_with_for_loop/02_digits_again.js
@@ -1,3 +1,10 @@
 // ## Build an array
-//
+
+const digits = [];
+
 // Create the famous `digits` array using a `for-loop`.
+
+for (let i = 0; i < 10; i++ ){
+  digits.push(i);
+}
+console.log(digits);
