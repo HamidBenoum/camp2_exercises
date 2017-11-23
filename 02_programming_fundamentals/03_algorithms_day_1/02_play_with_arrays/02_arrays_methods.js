098f544ff1223945c71a2f51095eab243aa1a57c
Hamid Ben oumaizina <hamid.benoumaizina@decathlon.com>
2017-11-23 13:22:24 +0100

    mon exo du jour sur les variables et les arrays

--- 02_programming_fundamentals/03_algorithms_day_1/02_play_with_arrays/02_arrays_methods.js
+++ 02_programming_fundamentals/03_algorithms_day_1/02_play_with_arrays/02_arrays_methods.js
@@ -12,3 +12,29 @@
 
 
 // use `join` to create, into the variable `allDigits`, a string like this : `zero - one ...`.
+//const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const digits = [];
+
+/*
+//2. Play with arrays
+const first = digits[0];
+console.log(first);
+
+const last = digits[9];
+console.log(last);
+
+const sixt = digits[6];
+console.log(sixt);
+*/
+for (let i = 0; i < 10; i ++){
+  digits.push(i);
+}
+console.log(digits);
+
+const last = digits.length -1;
+console.log(last);
+
+const litteralDigits = ['zero', 'one', 'two', 'tree', 'four', 'five', 'six', 'height', 'nine'];
+
+const allDigits = litteralDigits.join('-');
+console.log(allDigits);
