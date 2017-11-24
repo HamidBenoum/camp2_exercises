e7e247fdd56ca05ad27697e5648c853396bf0baf
Hamid Ben oumaizina <hamid.benoumaizina@decathlon.com>
2017-11-24 18:26:11 +0100

    exo car rental

--- 02_programming_fundamentals/04_algorithms_day_2/02_car_rental/01_index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/02_car_rental/01_index.js
@@ -1,17 +1,52 @@
 const driver = {
-  driverLicense: "B1",
+  driverLicense: "B",
   licenseIssued: new Date().getFullYear() - 10, // 10 years old license
   numberOfAccident: 0,
-  bonus: 0.8,
+  bonus: 0.9,
 };
 
 // Write a function canRentACar:
 // * Input: a driver
 // * Output: a boolean if the driver can rent a car
+let thisGuyCanRentACar;
 
-
-
+function canRentACar(driver) {
+  //Check license
+  if (driver.driverLicense === "B" || driver.driverLicense === "B1") {
+    if (driver.licenseIssued <= new Date().getFullYear() - 2) {
+      if (driver.numberOfAccident === 0 || driver.bonus >= 0.7) {
+        thisGuyCanRentACar = true;
+      }
+    } else {
+      thisGuyCanRentACar = false;
+    }
+  } else {
+    thisGuyCanRentACar = false;
+  }
+  return thisGuyCanRentACar;
+}
+canRentACar(driver);
 
 // ⚠ Do not remove me ! It's for tests
 // eslint-disable-next-line
 module.exports = canRentACar
+/*
+//Check license
+if (driver.driverLicense === "B" || driver.driverLicense === "B1") {
+  thisGuyCanRentACar = true;
+}
+//Check licenceYear
+if (driver.licenseIssued <= 2015) {
+  thisGuyCanRentACar = true;
+}
+//Check AccidentCount
+if (driver.numberOfAccident === 0) {
+  thisGuyCanRentACar = true;
+}
+//Check bonus
+if (driver.bonus >= 0.7) {
+  thisGuyCanRentACar = true;
+}
+return thisGuyCanRentACar;
+licenseIssued: new Date().getFullYear() - 10, // 10 years old license
+*/
