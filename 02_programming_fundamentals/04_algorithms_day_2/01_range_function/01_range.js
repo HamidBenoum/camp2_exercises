2e2e4aa22c9519b30bdd2f74b0d9f8cf64be7b0f
Hamid Ben oumaizina <hamid.benoumaizina@decathlon.com>
2017-11-24 15:35:51 +0100

    Fonction range avec tri

--- 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
+++ 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
@@ -4,9 +4,28 @@
 // This function should work in both ascending or descending order.
 
 // Complete this function.
-function range() {
 
+
+
+
+function range(parameter1, parameter2) {
+let myArray = [];
+
+
+  if (parameter1 < parameter2) {
+    // cas ascendant
+    for (let i = parameter1; i <= parameter2; i++) {
+      myArray.push(i);
+    }
+  } else {
+    for (let i = parameter1; i >= parameter2; i--) {
+      myArray.push(i);
+    }
+  }
+  return myArray;
 }
+
+range(4,1);
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
