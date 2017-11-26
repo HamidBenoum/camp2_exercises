a25492d9e27f0f557c1125c674e8a3109790de10
Hamid Ben oumaizina <hamid.benoumaizina@decathlon.com>
2017-11-26 13:06:52 +0100

    Exo fizzbuzz finished

--- 02_programming_fundamentals/04_algorithms_day_2/03_fizz_buzz/fizz_buzz.js
+++ 02_programming_fundamentals/04_algorithms_day_2/03_fizz_buzz/fizz_buzz.js
@@ -1,16 +1,24 @@
-/* Implement a fizzBuzz function as such: `fizzBuzz(list)`, where `list` is an array
-   of positive integers, for example : `[1, 2, 3, 4, 5, 6]`.
-   This function will return a new array where some values will have been modified:
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
-   - if the number is divisible by 3: `Fizz`;
-   - if the number is divisible by 5: `Buzz`;
-   - if the number is divisible by 3 and 5 : `FizzBuzz`
-   - otherwise, the value is preserved.
-*/
 
 function fizzBuzz(list) {
-
+  const fizzbuzzed = [];
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] % 3 === 0 && list[i] % 5 === 0) {
+      fizzbuzzed.push("FizzBuzz");
+    } else if (list[i] % 3 === 0) {
+      fizzbuzzed.push("Fizz");
+    } else if (list[i] % 5 === 0) {
+      fizzbuzzed.push("Buzz");
+    } else {
+      fizzbuzzed.push(list[i]);
+    }
+  }
+  return fizzbuzzed;
 }
+
+const result = fizzBuzz(list);
+console.log(result);
 
 
 module.exports = fizzBuzz;
