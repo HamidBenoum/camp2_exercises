2074b28b26b6847a95756ec99c314357300918d4
Hamid Ben oumaizina <hamid.benoumaizina@decathlon.com>
2017-11-27 11:38:47 +0100

    weekend exo

--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/01_filter_with_string.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/01_filter_with_string.js
@@ -1,8 +1,32 @@
 // filter takes an array of integer and a string with either odd or even
 // such as filter([1, 2, 3, 4, 5], 'even') returns [2, 4]
+const list = [1, 2, 3, 4, 5];
+
 function filter(array, str) {
-  // Your code here
+  const filteredList = [];
+
+  if (str === "even") {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] % 2 === 0) {
+        filteredList.push(array[i]);
+      }
+    }
+  } else if (str === "odd") {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] % 2 !== 0) {
+        filteredList.push(array[i]);
+      }
+    }
+  }
+
+  return filteredList;
 }
+
+const result = filter([-1, -2, -3, -4, -5], "toto");
+console.log(result);
+
+
+
 
 // do not remove this line, it is for tests
 module.exports = filter;
