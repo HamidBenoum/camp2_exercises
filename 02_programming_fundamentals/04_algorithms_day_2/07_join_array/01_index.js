e3352b9903584ab479938f3d60e272f0cd7d153d
Hamid Ben oumaizina <hamid.benoumaizina@decathlon.com>
2017-11-28 16:19:59 +0100

    How to use reduce

--- 02_programming_fundamentals/04_algorithms_day_2/07_join_array/01_index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/07_join_array/01_index.js
@@ -5,6 +5,19 @@
 // eg: join(["zero", "one", "two"], "-") => "zero - one - two"
 
 
+const list = ["zero", "one", "two"];
+
+
+function joinArray (array, str) {
+  const result = array.reduce((accumulator, currentValue) => accumulator + str + currentValue);
+  return result;
+}
+
+joinArray(list,"-");
+
+
+
+
 // ⚠ Do not remove me ! It's for tests
 // eslint-disable-next-line
 module.exports = joinArray;
