b0ca622cee7670db8ffd96d4b1b152abe39a743d
Hamid Ben oumaizina <hamid.benoumaizina@decathlon.com>
2017-11-28 10:59:11 +0100

    Morse cod done

--- 02_programming_fundamentals/05_callbacks/05_morse_code/morse_code.js
+++ 02_programming_fundamentals/05_callbacks/05_morse_code/morse_code.js
@@ -44,9 +44,43 @@ const MORSE_CODE = {
   "----.": "9"
 };
 
+// decodeMorse(".... . -.--   .--- ..- -.. .")
+
+let sentence = ".... . -.--   .--- ..- -.. .";
+
 function decodeMorse(morse) {
-  // Your code here
+  //on split code(sentence) en array avec coomme séparateur 3 espaces.
+  const mySentenceinArray = morse.split("   ");
+
+  /*on retourne mySentenceinArray avec deux elements, puis envoie le tout à la fonction
+  wordCheck
+  */
+  return mySentenceinArray
+    .map((word) => wordCheck(word))
+    .join(" ");
 }
+
+function wordCheck(word) {
+  //on split Word en array avec coomme séparateur 1 espace.
+  const myWordInArray = word.split(" ");
+
+  /*on retourne myWordInArray avec deux elements, puis envoie le tout à la fonction
+  letterDiscovery
+  */
+  return myWordInArray
+    .map((letter) => letterDiscovery(letter))
+    .join("");
+}
+
+function letterDiscovery(letter) {
+  // on fait correspondre les lettre au équivalent de l'objet Morse_code.
+  return MORSE_CODE[letter];
+}
+
+
+//phrase = phrase.toLowerCase().replace(/[^a-z]/g, "");
+//c = phrase.charCodeAt(i);
+
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
