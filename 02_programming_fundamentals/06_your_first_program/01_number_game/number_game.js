5613fb753df11793c551787cafcca081b55f1e87
Hamid Ben oumaizina <hamid.benoumaizina@decathlon.com>
2017-11-28 15:01:39 +0100

    Number game ok

---
+++ 02_programming_fundamentals/06_your_first_program/01_number_game/number_game.js
@@ -0,0 +1,31 @@
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+//generation d'un nombre aléatoir ente 0 et 100.
+function randomNumberBetween0and100(){
+  return Math.floor(Math.random()*100);
+}
+
+function numberGame() {
+  let hiddenValue = randomNumberBetween0and100();
+  reader.question("devine un nombre : ", function(number){
+    let userNumber = parseInt(number, 10);
+    if (number !== hiddenValue) {
+      if(number > hiddenValue ){
+        console.log("Too high");
+      } else if (number < hiddenValue) {
+        console.log("Too low");
+      }
+      numberGame();
+    }else {
+      console.log("You won!");
+      reader.close();
+    }
+  });
+}
+
+numberGame();
