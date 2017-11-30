94cf5ba8be836a8d4ea621710304c559a0d85b0c
Hamid Ben oumaizina <hamid.benoumaizina@decathlon.com>
2017-11-30 08:54:06 +0100

    exos about loops and pyramid

--- 02_programming_fundamentals/03_algorithms_day_1/05_play_with_for_loop_bonus/05_pyramid.js
+++ 02_programming_fundamentals/03_algorithms_day_1/05_play_with_for_loop_bonus/05_pyramid.js
@@ -6,3 +6,18 @@
 //  *****
 // *******
 // ```
+
+
+let str = "*******";
+let output="";
+let myStr ="";
+
+for (let i = str.length; i >= 0; i--) {
+  if (i % 2 === 0) {
+    myStr = " ".repeat((i - 1)/2 + 1);
+    output = [str.slice(i, 0 ), myStr, str.slice(i - str.length)].join("");
+    console.log(output);
+
+  }
+
+}
