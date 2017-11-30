7e104caed8e88f63b858fd44f9ad5c47c21b3ae3
Hamid Ben oumaizina <hamid.benoumaizina@decathlon.com>
2017-11-30 14:30:41 +0100

    Ze coffe machine done

--- 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
+++ 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
@@ -2,6 +2,37 @@
 
 // Coffee Machine usage. Insert your code above this comment
 
+const machine = {
+  litersOfCoffee : 0,
+
+  fillWithLitersOfCoffee: function (param){
+    this.litersOfCoffee = this.litersOfCoffee + param;
+  },
+  expresso : function(){
+    //volume café pour un expresso : 0.08
+    //stock café ok sinon false
+    if (this.litersOfCoffee >= 0.08) {
+      this.litersOfCoffee = this.litersOfCoffee - 0.08;
+      return true;
+    } else {
+      return false;
+    }
+  },
+  longCoffee : function() {
+    //volume café pour un longExpresso : 0.23
+    //stock café ok sinon false
+    if (this.litersOfCoffee >= 0.15) {
+      this.litersOfCoffee = this.litersOfCoffee - 0.15;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+
+
+
 machine.fillWithLitersOfCoffee(10);
 console.log(machine.expresso()) // => true
 console.log(machine.litersOfCoffee) // => 9.92
@@ -10,5 +41,8 @@ console.log(machine.litersOfCoffee) // => 9.77
 // some more people ordering coffee here
 console.log(machine.litersOfCoffee) // => 0.02
 console.log(machine.expresso()) // => false
+
+
+
 
 module.exports = machine;
