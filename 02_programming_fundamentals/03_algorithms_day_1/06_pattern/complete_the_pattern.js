d0a86c9ee44c6d137f803b145ba73b5385912c75
Hamid Ben oumaizina <hamid.benoumaizina@decathlon.com>
2017-12-11 09:06:07 +0100

    pattern completeted

--- 02_programming_fundamentals/03_algorithms_day_1/06_pattern/complete_the_pattern.js
+++ 02_programming_fundamentals/03_algorithms_day_1/06_pattern/complete_the_pattern.js
@@ -1,3 +1,18 @@
 // Modify this value to test with other values
-const numberOfLine = 5;
+const numberOfLine = 4;
 // Your code here ⬇
+
+function pattern(numberOfLine) {
+  let myArray = [];
+  if (numberOfLine > 0) {
+    myArray = (Array.from(Array(numberOfLine + 1).keys())).reverse();
+    for (let i = numberOfLine; i > 0; i--) {
+      myArray.pop();
+      console.log(myArray.join(""));
+    }
+  } else {
+    return myArray;
+  }
+}
+
+pattern(numberOfLine);
