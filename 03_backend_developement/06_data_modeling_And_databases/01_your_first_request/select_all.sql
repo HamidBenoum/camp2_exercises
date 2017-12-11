13f0e78788430be1354eb7d3d561833057ceff69
hamid ben oumaizina <hamid.benoumaizina@decathlon.com>
2017-12-11 11:48:23 +0100

    trying to solve my stuff

---
+++ 03_backend_developement/06_data_modeling_And_databases/01_your_first_request/select_all.sql
@@ -0,0 +1,5 @@
+-- Make a request that select all rows from the table `persons`
+-- When you think you have finished, you can run `npm test` in this directory to check the answer
+
+
+-- SPARTA_API_KEY="P6eA1tZCnftUvX2cDGnqew==" EMAIL="hamid.benoumaizina@decathlon.com" NAME="hamid ben oumaizina" rcup -f -d ~/Workspace/dotfiles
